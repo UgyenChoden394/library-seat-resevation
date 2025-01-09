99362ccc849b42051530578ea8e224fe8f757db4
UgyenChoden394 <ugyenchoden972@gmail.com>
2025-01-09 19:21:16 +0600

    dashboard layout changed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,21 +2,49 @@
 import Header from '@/components/header';
 import Sidebar from '@/components/sidebar';
 import { redirect } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { fetchCurrentUserData } from '../firebase/authConfig';
+import Loader from '@/components/loader';
 
 export default function Layout({children} : {children: React.ReactNode}) {
     const userSession = sessionStorage.getItem('user');
+    const [userData, setUserData] = useState({ student_id: '', role: '' });
+    const [loading, setLoading] = useState(true);
     if (!userSession) {
         redirect('/auth/login')
     }
+
+    useEffect(() => {
+      const fetchUserData = async () => { 
+          try {
+            const currentUser = await fetchCurrentUserData();
+            if(currentUser) { 
+              setUserData(currentUser);
+              console.log('current User', currentUser)
+            }
+          } catch (error) {
+            console.error("Error fetching user data:", error);
+          } finally {
+            setLoading(false);
+          }
+      };
+      fetchUserData();
+    }, []);
+
+    if (loading) {
+      return <Loader></Loader>;
+    }
+    
     return (
         <div className='flex h-screen'>
+
           {/* Sidebar */}
-          <Sidebar></Sidebar>
+          <Sidebar userData={userData} ></Sidebar>
 
            {/* Main Content */}
            <div className="flex-1 flex flex-col bg-gray-100">
             {/* Header */}
-            <Header />
+            <Header userData={userData}/>
 
             {/* Page Content */}
             <main className="flex-1 p-6">{children}</main>

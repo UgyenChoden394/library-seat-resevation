2444c3dc01e35ba052a930610173ea6f98e66e7c
UgyenChoden394 <ugyenchoden972@gmail.com>
2025-01-16 14:11:58 +0600

    updated for dynamic modal value

--- src/components/confirmationDialog.tsx
+++ src/components/confirmationDialog.tsx
@@ -1,5 +1,5 @@
-export default function ConfirmationDialog({isOpen, onConfirm, onCancel, message, imageUrl}: 
-    { isOpen: boolean; onConfirm: () => void; onCancel: () => void ; message: string, imageUrl?: string;})
+export default function ConfirmationDialog({isOpen, onConfirm, onCancel, message, imageUrl, confirmMsg, customClass}: 
+    { isOpen: boolean; onConfirm: () => void; onCancel: () => void ; message: string, imageUrl?: string; confirmMsg?: string, customClass?:string})
 {
     if(!isOpen) return null;
 
@@ -8,11 +8,12 @@ export default function ConfirmationDialog({isOpen, onConfirm, onCancel, message
         <div className="bg-white p-6 rounded-lg shadow-lg">
           <h2 className="text-xl font-semibold mb-4">{message}</h2>
           <div className='flex justify-center items-center my-4'>
-           {imageUrl && <img src={imageUrl} alt='Image To Delete' className='w-[250px] h-[270px]' />}
+           {imageUrl && <img src={imageUrl} alt='Image To Delete' className='w-[250px] h-[270px] rounded-2xl object-cover' />}
           </div>
-          <div className="flex justify-between">
-            <button className="bg-red-500 text-white px-4 py-2 rounded-lg" onClick={onConfirm}>
-              Confirm Delete
+          <div className="flex justify-between mt-8">
+            <button className={` px-4 py-2 rounded-lg ${customClass ? customClass : 'bg-red-500 text-white'}`} 
+              onClick={onConfirm}>
+              {confirmMsg ? confirmMsg  : 'Confirm Delete'}
             </button>
             <button className="bg-gray-300 px-4 py-2 rounded-lg" onClick={onCancel}>
               Cancel
